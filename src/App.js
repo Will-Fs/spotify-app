f29ca5269f22dcd1e992bb247ddd5d7c84618e88
Will <will.fs.martian@gmail.com>
2022-09-27 09:52:44 -0400

    Top Artist

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { FastAverageColor } from 'fast-average-color';
 SpotifyWebApi._addMethods(SpotifyWebApiServer);
 
 const redirect_uri = "http://localhost:3000/";
+const formatter = Intl.NumberFormat("en", {notation: 'compact'});
 var auth_code;
 
 const fac = new FastAverageColor();
@@ -20,7 +21,7 @@ const state = 'spotify-web-app';
 const authorize_url = api.createAuthorizeURL(
   scopes, 
   state,
-  true,
+  false,
   'token'
 );
 
@@ -40,7 +41,7 @@ export const displayUserInfo = () => {
     console.log(api.getAccessToken());
     return;
   }
-  const _displayUserInfo = (profileInfo, playlistData) => {
+  const _displayUserInfo = (profileInfo, playlistData, topArtistsData) => {
     const getColorInfo = color => {
       const bgColorMult = 0.3
       return (
@@ -53,28 +54,39 @@ export const displayUserInfo = () => {
       );
     }
     console.log(profileInfo);
+    console.log(topArtistsData);
 
     const content_container = document.querySelector(".content-container");
     const root = ReactDOM.createRoot(content_container);
     const pfpUrl = profileInfo.images[0].url;
+
+    let pfpColors, artistColors;
+
     fac.getColorAsync(pfpUrl)
       .then(color => {
-        return getColorInfo(color);
+        pfpColors = getColorInfo(color);
+        return fac.getColorAsync(topArtistsData.topArtist.image.url)
     })
-    .then(pfpColors => {
-
+    .then (color => {
+      artistColors = getColorInfo(color);
+      const topArtist = topArtistsData.topArtist;
         const content = 
         <div className = "user-info">
-          <div className="profile" style={{backgroundImage: `linear-gradient(to top, ${pfpColors.bottomColor}, ${pfpColors.topColor}`, filter: "saturate(2)"}}>
-            <img id='display-img' src={pfpUrl ? pfpUrl : ""} alt="Spotify Profile" style={{filter: "saturate(0.5)"}}></img>
+          <div className="profile" id="user" style={{backgroundImage: `linear-gradient(to top, ${pfpColors.bottomColor}, ${pfpColors.topColor}`, filter: "saturate(2)"}}>
+            <img id='profile-img' src={pfpUrl ? pfpUrl : ""} alt="Spotify Profile" style={{filter: "saturate(0.5)"}}></img>
             <h1 id='name' style={{color: pfpColors.foregroundColor}}>{profileInfo.display_name}</h1>
             <div className="profile-stats">
-              <p id="follower-count" style={{color: pfpColors.foregroundColor}}>{profileInfo.followers.total} Followers</p>
+              <p id="follower-count" style={{color: pfpColors.foregroundColor}}>{formatter.format(profileInfo.followers.total)} Followers</p>
               <p id="public-playlist-count" style={{color: pfpColors.foregroundColor}}>{playlistData.numPublicPlaylists} Public Playlists</p>
             </div>
           </div>
-          <div className='top-artist'>
-
+          <div className="profile" id="top-artist" style={{backgroundImage: `linear-gradient(to top, ${artistColors.bottomColor}, ${artistColors.topColor}`, filter: "saturate(2)"}}>
+            <img id='profile-img' src={topArtist.image.url} alt="Spotify Profile" style={{filter: "saturate(0.5)"}}></img>
+            <h1 id='name' style={{color: artistColors.foregroundColor}}>{topArtistsData.topArtist.name}</h1>
+            <div className="profile-stats">
+              <p id="top-artist-label">Top Artist this Month</p>
+              <p id="follower-count" style={{color: artistColors.foregroundColor}}>{formatter.format(topArtist.followerCount)} Followers</p>
+            </div>
           </div>
         </div>;
 
@@ -82,7 +94,7 @@ export const displayUserInfo = () => {
       })
   }
 
-  let userData, playlistData;
+  let userData, playlistData, topArtistsData;
 
   api
     .getMe()
@@ -92,7 +104,6 @@ export const displayUserInfo = () => {
     })
     .then(data => {
       const publicPlaylists = data.body.items.filter(playlist => playlist.public);
-      console.log(data.body);
 
       playlistData = {
         publicPlaylists: publicPlaylists,
@@ -101,8 +112,19 @@ export const displayUserInfo = () => {
       return api.getMyTopArtists({ time_range: "short_term" });
     })
     .then (data => {
-      console.log(data);
-      _displayUserInfo(userData, playlistData);
+      data = data.body;
+      const topArtist = data.items[0];
+
+      const topArtistData = {
+        name: topArtist.name,
+        followerCount: topArtist.followers.total,
+        image: topArtist.images[0],
+        link: topArtist.external_urls.spotify
+      }
+
+      topArtistsData = {list: data.items, topArtist: topArtistData};
+
+      _displayUserInfo(userData, playlistData, topArtistsData);
     })
     .catch(err => {
       console.log(`Something went wrong: ${err}`);
