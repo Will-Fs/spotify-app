52c0d8712b0389f903f469020ef2e24801fccbb4
Will <will.fs.martian@gmail.com>
2022-10-03 02:06:09 -0400

    More stats, better colors

--- src/components/mainObjects/MeObject.js
+++ src/components/mainObjects/MeObject.js
@@ -2,12 +2,13 @@ import { api } from "../../spotify"
 import { InfoCard } from "../InfoCard";
 
 export const renderMe = (root) => {
-    let topArtist;
+    let topArtists = {};
+    let topTracks = {};
     let error = null;
     api
     .getMyTopArtists({ time_range: "short_term" })
       .then(data => {
-        topArtist = data.body.items[0];
+        topArtists.short = data.body.items[0];
         return api.getMyTopTracks({time_range: "short_term"});
       }, err => {
        error = err;
@@ -18,15 +19,28 @@ export const renderMe = (root) => {
           root.render(data);
           return;
         }
-        const topTrack = data.body.items[0];
+      
+        topTracks.short = data.body.items[0];
+
+        return api.getMyTopTracks({time_range: "long_term"});
+      })
+      .then (data => {
+        topTracks.long = data.body.items[0];
+        return api.getMyTopArtists({ time_range: "long_term" })
+      })
+      .then (data => {
+        topArtists.long = data.body.items[0];
+
         const content = 
         <div className="object-info">
             <InfoCard type="me" me={true}></InfoCard>
-            <InfoCard type="artist" id={topArtist.id} isTopArtist={true} timeFrame={"short"}></InfoCard>
-            <InfoCard type="track" id={topTrack.id} isTopTrack={true} timeFrame={"short"}></InfoCard>
+            <div className="secondary-object-info">
+              <InfoCard type="artist" id={topArtists.short.id} isTopArtist={true} timeFrame={"short"}></InfoCard>
+              <InfoCard type="track" id={topTracks.short.id} isTopTrack={true} timeFrame={"short"}></InfoCard>
+              <InfoCard type="artist" id={topArtists.long.id} isTopArtist={true} timeFrame={"long"}></InfoCard>
+              <InfoCard type="track" id={topTracks.long.id} isTopTrack={true} timeFrame={"long"}></InfoCard>
+            </div>
         </div>;
         root.render(content);
-      }, err => {
-        console.log(err);
       });
 }
--- src/utility/colorAnalyzer.js
+++ src/utility/colorAnalyzer.js
@@ -3,15 +3,16 @@ import * as Vibrant from "node-vibrant";
 
 export const getColorInfo = color => {
     // console.log(color);
-    color = color.LightVibrant.getRgb();
+    color = color.Vibrant.getRgb();
     const bgColorMult = 0.2;
+    const opacity = 0.5;
     const avgColor = color.slice(0, 3).reduce((a, b) => a + b) / 3;
     return (
       {
         foregroundColor: avgColor > 120 ? "black" : "white",
         bgColorMult: bgColorMult,
-        topColor: `rgb(${color.map(color => color * (1 + bgColorMult * 2)).join(", ")})`,
-        bottomColor: `rgb(${color.map(color => color * (1 - bgColorMult / 3)).join(", ")})`,
+        topColor: `rgba(${color.map(color => color * (1 + bgColorMult * 2)).join(", ")}, ${opacity})`,
+        bottomColor: `rgba(${color.map(color => color * (1 - bgColorMult / 3)).join(", ")},  ${opacity})`,
         bgTopColor: `rgb(${color.map(color => (color * 130/avgColor) ** 1.1).join(", ")})`
       }
     );
