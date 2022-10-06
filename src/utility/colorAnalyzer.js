c91deb8b84cbaf4fd41129d4c825e666402773b9
Will <will.fs.martian@gmail.com>
2022-10-05 23:05:03 -0400

    ColorAnalyzer css variables

--- src/utility/colorAnalyzer.js
+++ src/utility/colorAnalyzer.js
@@ -1,20 +1,21 @@
 import * as Vibrant from "node-vibrant";
 
+const usePageColors = true;
+const colorThreshold = 255;
+const bgColorMult = 0;
+const opacity = 1;
 
 export const getColorInfo = color => {
     // console.log(color);
     color = color.LightVibrant.getRgb();
-    const bgColorMult = 0;
-    const opacity = 1;
     const avgColor = color.slice(0, 3).reduce((a, b) => a + b) / 3;
-    const colorThreshold = 255;
     return (
       {
         foregroundColor: avgColor > colorThreshold ? "black" : "white",
         secondaryColor: avgColor > colorThreshold ? "rgb(30, 30, 30)" : "rgb(180, 180, 180)",
         bgColorMult: bgColorMult,
-        topColor: "var(--body-secondary-bg-color)",//`rgba(${color.map(color => color * (1 + bgColorMult * 2)).join(", ")}, ${opacity})`,
-        bottomColor: "var(--body-secondary-bg-color)",//`rgba(${color.map(color => color * (1)).join(", ")},  ${opacity})`,
+        topColor: usePageColors ? "var(--body-secondary-bg-color)" : `rgba(${color.map(color => color * (1 + bgColorMult * 2)).join(", ")}, ${opacity})`,
+        bottomColor: usePageColors ? "var(--body-secondary-bg-color)" : `rgba(${color.map(color => color * (1)).join(", ")},  ${opacity})`,
         bgTopColor: `rgb(${color.map(color => (color * 130/avgColor) ** 1.1).join(", ")})`
       }
     );
