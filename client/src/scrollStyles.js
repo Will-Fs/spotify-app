9aa487ff213f54fa1bfc1da8d7e0a1da00c32f1c
Will <will.fs.martian@gmail.com>
2022-10-13 01:31:32 -0400

    scroll style fix

--- client/src/scrollStyles.js
+++ client/src/scrollStyles.js
@@ -2,13 +2,15 @@ export const scrollStyles = () => {
     const objectContainer = document.querySelector(".object-info-container");
     // console.log(objectContainer.style.height)
     const tracksHeader = document.querySelector(".tracks-header");
-    if (window.scrollY > objectContainer.clientHeight + 70) {
-        tracksHeader.style = "\
-            background-color: rgba(var(--body-bg-color-value), 0.8);\
-            backdrop-filter: blur(3px);"
-    }
-    else {
-        tracksHeader.style = "none";
+    if (tracksHeader) {
+        if (window.scrollY > objectContainer.clientHeight + 70) {
+            tracksHeader.style = "\
+                background-color: rgba(var(--body-bg-color-value), 0.8);\
+                backdrop-filter: blur(3px);"
+        }
+        else {
+            tracksHeader.style = "none";
+        }
     }
     
 //   
